c371d86aa11e0c10c215591302f6cbcbb1b6474d
Md Feroj Bepari <feroj21@gmail.com>
2023-08-28 14:21:05 -0400

    Update reversed_singly_linked_list.js

--- reversed_singly_linked_list.js
+++ reversed_singly_linked_list.js
@@ -1,33 +1,65 @@
 // Time Complexity: O(N)
 // Space Complexity: O(N)
 class Node {
-  constructor(data) {
-    this.data = data
-    this.next = null                
-  }
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+function reverse(head) {
+    let currentNode = head;
+
+    let prevNode = null;
+
+    while (currentNode) {
+        let next = currentNode.next;
+        currentNode.next = prevNode;
+        prevNode = currentNode;
+        currentNode = next;
+    }
+
+    return prevNode;
 }
 
 function reversedLinkedList(head, left, right) {
-  let prevNode = null;
-  let nextNode = null;
-  
-  let headNode = head;
-  let leftPointer = 1;
-  while(leftPointer < left) {
-    headNode = headNode.next;
-    leftPointer++;
-  }
+    if (left === right) {
+        return head;
+    }
 
-  let currentNode = headNode;
+    let revStart = null;
+    let revStartPrevious = null;
+    let revEnd = null;
+    let revEndNext = null;
+    let currentNode = head;
 
-  while(currentNode) {
-    nextNode = currentNode.next;
-    currentNode.next = prevNode;
-    prevNode = currentNode;
-    currentNode = nextNode;
-    nextNode = null;
-  }
+    let pointer = 1;
+    while (currentNode && pointer <= right) {
+        if (pointer < left)
+            revStartPrevious = currentNode;
 
-  head = prevNode;
-  return head;
+        if (pointer === left)
+            revStart = currentNode;
+
+        if (pointer === right) {
+            revEnd = currentNode;
+            revEndNext = currentNode.next;
+        }
+
+        currentNode = currentNode.next;
+        pointer++;
+    }
+
+    revEnd.next = null;
+
+    revEnd = reverse(revStart);
+
+    if (revStartPrevious) {
+        revStartPrevious.next = revEnd;
+    } else {
+        head = revEnd;
+    }
+
+    revStart.next = revEndNext;
+    return head;
 }
