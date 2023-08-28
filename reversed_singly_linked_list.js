13aef2d900e0d014ac2d2b22face1e75622dd187
Md Feroj Bepari <feroj21@gmail.com>
2023-08-28 13:58:44 -0400

    Update reversed_singly_linked_list.js

--- reversed_singly_linked_list.js
+++ reversed_singly_linked_list.js
@@ -1,9 +1,5 @@
-class LinkedList {
-  constructor(head = null) {
-      this.head = head
-  }
-}
-
+// Time Complexity: O(N)
+// Space Complexity: O(N)
 class Node {
   constructor(data) {
     this.data = data
@@ -36,20 +32,3 @@ function reversedLinkedList(head, left, right) {
   head = prevNode;
   return head;
 }
-
-let node1 = new Node(1)
-let node2 = new Node(2)
-let node3 = new Node(3)
-let node4 = new Node(4)
-let node5 = new Node(5)
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
-
-let head = reversedLinkedList(node1, 2,4);
-
-
-let list = new LinkedList(head)
-
-console.log(list.head.data);
